27e1408460b24d6d4475e4a0f0026dc2a1c815a9
Yash200237 <yashodhathathsarani2002@gmail.com>
2025-01-17 20:58:36 +0530

    database hosted

--- Backend/database.py
+++ Backend/database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # MySQL connection string
-URL_DATABASE = "mysql+pymysql://root:yashodha@localhost:3306/smartfoodie"
+URL_DATABASE = os.getenv("URL_DATABASE")
 
-engine = create_engine(URL_DATABASE)
+engine = create_engine(
+    URL_DATABASE + "?ssl_ca=ca.pem"
+)
 
 # Create a session for database interaction
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
